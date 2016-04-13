c1f7c0265beaa4efc74fd29b2d79dc43cf3b83ed
EQuimper <quimperemanuel@gmail.com>
2016-04-13 14:57:21 -0600

    Finish up

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -9,7 +9,8 @@ class BookList extends Component {
 			return (
 				<li
 					className="list-group-item"
-					key={book.title}>
+					key={book.title}
+					onClick={() => this.props.selectBook(book)}>
 					{book.title}
 				</li>
 			)
@@ -32,10 +33,15 @@ function mapStateToProps(state) {
 	};
 }
 
+// Anything returned from this function will end up as props
+// on the BookList container
 function mapDispatchToProps(dispatch) {
 	// Whenever selectBook is called, the result should be passed
 	// to all of our reducers
 	return bindActionCreators({ selectBook: selectBook }, dispatch)
 }
 
+// Promote BookList from a component to a container - it need to know
+// about this new dispatch method, selectBook. Make it available
+// as a prop.
 export default connect(mapStateToProps, mapDispatchToProps)(BookList);
